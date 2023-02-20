bc6a9b10ba59aa2d3b0dc318e8cd754f9ccd4934
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-02-20 10:15:13 +0530

    Added LinkedListNode.java file

---
+++ LinkedListNode.java
@@ -0,0 +1,24 @@
+package com.linkedlist;
+
+import java.util.jar.Attributes.Name;
+
+public class LinkedListNode <T>{
+	public T data;
+	public Name next;
+	//constructor
+	public void Node(T data)
+	{
+		this.data = data;
+	}
+	//setters and getters
+	public T getData()
+	{
+		return data;
+	}
+	public void setData(T data)
+	{
+		this.data = data;
+	}
+}
+
+
