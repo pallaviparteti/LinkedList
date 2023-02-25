aaf71cd2c5af707f22c43e611ebc310a1a83ceb9
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-02-25 21:49:32 +0530

    Added: LinkedListDSMain.java file

--- LinkedListDSMain.java
+++ LinkedListDSMain.java
@@ -1,23 +1,24 @@
 package com.linkedlist;
-import java.util.*;
 
-public class LinkedListDSMain<T> {
+public class LinkedListDSMain {
 	public static void main(String[] args)
 	{
-		LinkedList<Integer>linkedListObj1 = new LinkedList<Integer>();
+
+		//	    public static void main(String[] args)
+		LinkedListService<Integer>linkedListObj1 = new LinkedListService<Integer>(20);
 		System.out.println("Create a linked list using add method");
-		System.out.println( linkedListObj1.add(56));
+		linkedListObj1.add(56);
 		linkedListObj1.add(30);
 		linkedListObj1.add(70);
 		linkedListObj1.display();
 		System.out.println("Create a linked list using append method");
-		LinkedList<Integer>linkedListObj2 = new LinkedList<Integer>();
+		LinkedListService<Integer>linkedListObj2 = new LinkedListService<Integer>(20);
 		linkedListObj2.append(56);
 		linkedListObj2.append(30);
 		linkedListObj2.append(70);
 		linkedListObj2.display();
 		System.out.println("Insert a node 30 between 56 and 70");
-		LinkedList<Integer>linkedListObj3 = new LinkedList<Integer>();
+		LinkedListService<Integer>linkedListObj3 = new LinkedListService<Integer>(20);
 		linkedListObj3.append(56);
 		linkedListObj3.append(70);
 		linkedListObj3.insertAfter(linkedListObj3.head,30);
@@ -26,7 +27,7 @@ public class LinkedListDSMain<T> {
 		linkedListObj3.delete();
 		linkedListObj3.display();
 		System.out.println("Delete the last element in the linked list");
-		linkedListObj2.deleteLast();
+		linkedListObj2.popLast();
 		linkedListObj2.display();
 		System.out.println("Search for a node with key 30 in the linked list");
 		linkedListObj1.search(30);
